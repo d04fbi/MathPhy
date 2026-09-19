f207ee8978b4299c4caacb56c07244e04fad0a78
Fredrik Bierich <fredrik.bierich@gmail.com>
2026-09-19 13:24:04 +0200

    Add files via upload

---
+++ startup.m
@@ -0,0 +1,15 @@
+% Copyright © 2026 Fredrik Bierich
+% SPDX-License-Identifier: MIT
+% Licensed under the MIT License. See the root LICENSE file for details.
+set(groot, 'DefaultAxesColorOrder', [0, 0, 0]);
+set(groot, 'DefaultAxesFontSize', 12);
+set(groot, 'DefaultAxesTickLabelInterpreter', 'latex');
+set(groot, 'DefaultPatchEdgeColor', [0, 0, 0]);
+set(groot, 'DefaultPatchFaceColor', [0, 0, 0]);
+set(groot, 'DefaultRectangleCurvature', [1, 1]);
+set(groot, 'DefaultRectangleEdgeColor', [0, 0, 0]);
+set(groot, 'DefaultRectangleFaceColor', [0, 0, 0]);
+set(groot, 'DefaultTextFontSize', 12);
+set(groot, 'DefaultTextInterpreter', 'latex');
+set(groot, 'DefaultTextarrowshapeFontSize', 12);
+set(groot, 'DefaultTextarrowshapeInterpreter', 'latex');
