6fcd2256dc7974fa1d3f9ff30048d505c7d540bb
Fredrik Bierich <fredrik.bierich@gmail.com>
2026-09-21 19:26:43 +0200

    Delete exportvectorgraphics.m

--- exportvectorgraphics.m
+++
@@ -1,9 +0,0 @@
-% Copyright (c) 2026 Fredrik Bierich
-% SPDX-License-Identifier: MIT
-% Licensed under the MIT License. See the root LICENSE file for details.
-function exportvectorgraphics(varargin)
-    arguments (Repeating)
-        varargin
-    end
-    exportgraphics(varargin{:}, 'ContentType', 'vector');
-end
